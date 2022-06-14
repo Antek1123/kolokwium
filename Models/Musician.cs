c0a260c81bc67a7b1273612c546d3aad163085d9
Antoni Podębski <antoni.podebski@redge.com>
2022-06-14 12:21:56 +0200

    projekt

---
+++ Models/Musician.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolokwium.Models
+{
+    public class Musician
+    {
+        public int IdMusician { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+    }
+}
---
+++ Models/Musician_Track.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolokwium.Models
+{
+    public class Musician_Track
+    {
+        public int IdTrack { get; set; }
+        public int IdMusicia { get; set; }
+    }
+}
